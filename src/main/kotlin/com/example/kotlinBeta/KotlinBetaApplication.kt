4b33b891eed2fac55bca27ceb578d8a47bca8875
konstantin.nosenko <konstantin.nosenko@cosysoft.ru>
2020-01-09 17:31:18 +0400

    init

---
+++ src/main/kotlin/com/example/kotlinBeta/KotlinBetaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinBeta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBetaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinBetaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinBeta/KotlinBetaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinBeta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBetaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
